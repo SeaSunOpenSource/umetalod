dd0903128dab9e9e5e959ede070bf333370fb2ca
Gu Lu <gemfeeling@hotmail.com>
2015-07-17 17:15:49 +0800

    添加初始文件

---
+++ umetalod_test_unity5/Assets/umetalod/UMetaLod.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public struct UImpactFactor
+{
+    public string Name;
+    public float Weight;
+    public float UpperBound;
+    public float LowerBound;
+
+    public float Calculate(float value)
+    {
+        return 0.0f;
+    }
+}
+
+public class UHeatAttenuation
+{
+    public float DistInnerBound;
+    public float DistOuterBound;
+
+    public int PerfLevel;
+    public int FrameRate;
+
+    public float CalculateLiveness(float distance)
+    {
+        return 0.0f;
+    }
+}
+
+public interface IMetaLodTarget
+{
+    float GetDistance();
+
+    float GetFactorBounds();
+    float GetFactorGeomComplexity();
+    float GetFactorPSysComplexity();
+    float GetFactorVisualImpact();
+    float GetUserFactor(string factorName);
+
+    void SetLiveness(float liveness);
+}
+
+public class UMetaLod 
+{
+    public void AddTarget(IMetaLodTarget target)
+    {
+        if (!_targets.Contains(target))
+        {
+            _targets.Add(target);
+        }
+    }
+
+    public void AddUserFactor(UImpactFactor userFactor)
+    {
+        if (!_userFactors.Contains(userFactor))
+        {
+            _userFactors.Add(userFactor);
+        }
+    }
+
+    public void UpdateTargets()
+    {
+        foreach (var target in _targets)
+        {
+            UpdateLiveness(target);
+        }
+    }
+
+    private void UpdateLiveness(IMetaLodTarget target)
+    {
+        float distance = target.GetDistance();
+
+        float factorRate = 0.0f;
+        factorRate += _factorBounds.Calculate(target.GetFactorBounds());
+        factorRate += _factorBounds.Calculate(target.GetFactorBounds());
+        factorRate += _factorBounds.Calculate(target.GetFactorBounds());
+        factorRate += _factorBounds.Calculate(target.GetFactorBounds());
+
+        foreach (var factor in _userFactors)
+        {
+            factorRate += factor.Calculate(target.GetUserFactor(factor.Name));
+        }
+
+        distance *= 1.0f + factorRate;
+
+        float liveness = _heatAtte.CalculateLiveness(distance);
+        target.SetLiveness(liveness);
+    }
+
+    private HashSet<IMetaLodTarget> _targets = new HashSet<IMetaLodTarget>();
+    private UHeatAttenuation _heatAtte = new UHeatAttenuation();
+    
+    private HashSet<UImpactFactor> _userFactors;
+
+    private UImpactFactor _factorBounds = new UImpactFactor();
+    private UImpactFactor _factorGeomComplexity = new UImpactFactor();
+    private UImpactFactor _factorPSysComplexity = new UImpactFactor();
+    private UImpactFactor _factorVisualImpact = new UImpactFactor();
+}
---
+++ umetalod_test_unity5/Assets/umetalod/UMetaLodDef.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UMetaLodDef 
+{
+    // the bounding volume of the target
+    public static string Factor_Bounds = "Bounds";
+
+    // currently corresponds to vertex count of the target mesh, would be 0 for particle system
+    public static string Factor_GeomComplexity = "GeomComplexity";
+
+    // currently correspends to particle count of the target particle system, would be 0 for ordinary mesh
+    public static string Factor_PSysComplexity = "PSysComplexity";  
+    
+    // a subjective factor which reveals the visual importance of the target in some degrees
+    // for instance, skill effects casted by player would generally has a 
+    // pretty much higher visual impact than a static stone on the ground
+    public static string Factor_VisualImpact = "VisualImpact";
+
+}
